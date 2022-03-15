d68c85f8f780dd34513fc0e43e52989de9295d6f
tayel257 <tayel257@student.otago.ac.nz>
2022-03-15 15:00:33 +1300

    added a test comment

--- src/cosc202/andie/Andie.java
+++ src/cosc202/andie/Andie.java
@@ -35,7 +35,7 @@ public class Andie {
      * These operations are implemented {@link ImageOperation}s and triggerd via 
      * {@code ImageAction}s grouped by their general purpose into menus.
      * </p>
-     * 
+     * hi
      * @see ImagePanel
      * @see ImageAction
      * @see ImageOperation
