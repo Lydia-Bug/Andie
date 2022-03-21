1637fa92dad5cae2da01b99802ca796da1da1a42
Lydia Acton <acton.lydia@gmail.com>
2022-03-22 11:27:56 +1300

    Brightness filter working (just needs user input)

--- src/cosc202/andie/AdjustBrightness.java
+++ src/cosc202/andie/AdjustBrightness.java
@@ -41,15 +41,16 @@ public class AdjustBrightness implements ImageOperation, java.io.Serializable {
   
         for (int y = 0; y < input.getHeight(); ++y) {
             for (int x = 0; x < input.getWidth(); ++x) {
+                double brightness = 20;
                 int argb = input.getRGB(x, y);
                 int a = (argb & 0xFF000000) >> 24;
                 int r = (argb & 0x00FF0000) >> 16;
                 int g = (argb & 0x0000FF00) >> 8;
                 int b = (argb & 0x000000FF);
 
-                int newR = r + 20;
-                int newG = g + 20;
-                int newB = b + 20;
+                int newR = Math.min((int)Math.round((r-127.5)+127.5*(1+(brightness/100))),255);
+                int newG = Math.min((int)Math.round((g-127.5)+127.5*(1+(brightness/100))),255);
+                int newB = Math.min((int)Math.round((b-127.5)+127.5*(1+(brightness/100))),255);
 
                 argb = (a << 24) | (newR << 16) | (newG << 8) | newB;
                 input.setRGB(x, y, argb);
