6ce0b85308c60890f7b02f6c2c5b0b54455a66d2
tayel257 <tayel257@student.otago.ac.nz>
2022-03-29 15:36:21 +1300

    added option to rotate 90 degrees clockwise and anticlockwise to tranform dropdown.

--- src/cosc202/andie/TransformationActions.java
+++ src/cosc202/andie/TransformationActions.java
@@ -8,25 +8,49 @@ import javax.swing.*;
 
 public class TransformationActions {
 
-        /** A list of actions for the Resize, Rotate and Flip menu. */
-        protected ArrayList<Action> actions;
+    /** A list of actions for the Resize, Rotate and Flip menu. */
+    protected ArrayList<Action> actions;
 
-        /**
-         * <p>
-         * Creates list of menu actions.
-         * </p>
-         */
-        public TransformationActions() {
-            actions = new ArrayList<Action>();
-        }
 
-        public JMenu createMenu() {
-            JMenu fileMenu = new JMenu("Colour");
+    public TransformationActions() {
+        actions = new ArrayList<Action>();
+        actions.add(new Rotate90ClockwiseAction("Rotate 90° clockwise", null, null, null));
+        actions.add(new Rotate90AnticlockwiseAction("Rotate 90° anticlockwise", null, null, null));
+    }
+
+    public JMenu createMenu() {
+        JMenu fileMenu = new JMenu("Transform");
     
-            for(Action action: actions) {
-                fileMenu.add(new JMenuItem(action));
-            }
-    
-            return fileMenu;
+        for(Action action: actions) {
+            fileMenu.add(new JMenuItem(action));
         }
+    
+        return fileMenu;
+    }
+
+    public class Rotate90ClockwiseAction extends ImageAction {
+
+        Rotate90ClockwiseAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+            super(name, icon, desc, mnemonic);
+        }
+    
+        public void actionPerformed(ActionEvent e) {
+            target.getImage().apply(new Rotate90(90));
+            target.repaint();
+            target.getParent().revalidate();
+        }
+    }
+
+    public class Rotate90AnticlockwiseAction extends ImageAction {
+
+        Rotate90AnticlockwiseAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+            super(name, icon, desc, mnemonic);
+        }
+    
+        public void actionPerformed(ActionEvent e) {
+            target.getImage().apply(new Rotate90(270));
+            target.repaint();
+            target.getParent().revalidate();
+        }
+    }
 }
