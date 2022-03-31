bb37239a808b950127be16381bb37202f5bbd3e5
Hamzah Alansi <alaha971@student.otago.ac.nz>
2022-03-31 18:07:26 +1300

    ResizeAction (90% Done just need to make changes to scale factor)

--- src/cosc202/andie/Transformations/ResizeImage.java
+++ src/cosc202/andie/Transformations/ResizeImage.java
@@ -1,23 +1,40 @@
 //Skeleton class for resizing image - documentation to be added
 
 package cosc202.andie.Transformations;
+
+import java.awt.*;
 import java.awt.image.*;
+import java.lang.annotation.Target;
+import java.io.*;
+import javax.imageio.*;
 
 import cosc202.andie.ImageOperation;
 
 public class ResizeImage implements ImageOperation, java.io.Serializable {
+    private double scaleFactor;
 
-    private double newHeight;
-    private double newWidth;
+    public ResizeImage(double scaleFactor) {
+        this.scaleFactor = scaleFactor;
+        scaleFactor = scaleFactor / 1;
+        // this.newWidth = newWidth;
+    }
 
-    ResizeImage(double newHeight, double newWidth) {
-        this.newHeight = newHeight;
-        this.newWidth = newWidth;
+    ResizeImage() {
     }
 
     public BufferedImage apply(BufferedImage input) {
-        return input;
-    }
-    
-}
+        int width = input.getWidth();
+        int height = input.getHeight();
 
+        int newHeight = (int) (height * scaleFactor);
+        int newWidth = (int) (width * scaleFactor);
+
+        Image img = input.getScaledInstance(newWidth, newHeight, Image.SCALE_AREA_AVERAGING);
+        BufferedImage resizedImage = new BufferedImage(newWidth, newHeight, input.getType());
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(img, 0, 0, null);
+
+        return resizedImage;
+    }
+
+}
--- src/cosc202/andie/Transformations/TransformationActions.java
+++ src/cosc202/andie/Transformations/TransformationActions.java
@@ -13,22 +13,22 @@ public class TransformationActions {
     /** A list of actions for the Resize, Rotate and Flip menu. */
     protected ArrayList<Action> actions;
 
-
     public TransformationActions() {
         actions = new ArrayList<Action>();
         actions.add(new Rotate90ClockwiseAction("Rotate 90° clockwise", null, null, null));
         actions.add(new Rotate90AnticlockwiseAction("Rotate 90° anticlockwise", null, null, null));
         actions.add(new VerticalFlipAction("Flip vertically", null, null, null));
         actions.add(new HorizontalFlipAction("Flip horizontally", null, null, null));
+        actions.add(new ResizeAction("Resize", null, null, null));
     }
 
     public JMenu createMenu() {
         JMenu fileMenu = new JMenu("Transform");
-    
-        for(Action action: actions) {
+
+        for (Action action : actions) {
             fileMenu.add(new JMenuItem(action));
         }
-    
+
         return fileMenu;
     }
 
@@ -37,7 +37,7 @@ public class TransformationActions {
         Rotate90ClockwiseAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
             super(name, icon, desc, mnemonic);
         }
-    
+
         public void actionPerformed(ActionEvent e) {
             target.getImage().apply(new Rotate(90));
             target.repaint();
@@ -50,7 +50,7 @@ public class TransformationActions {
         Rotate90AnticlockwiseAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
             super(name, icon, desc, mnemonic);
         }
-    
+
         public void actionPerformed(ActionEvent e) {
             target.getImage().apply(new Rotate(270));
             target.repaint();
@@ -63,7 +63,7 @@ public class TransformationActions {
         VerticalFlipAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
             super(name, icon, desc, mnemonic);
         }
-    
+
         public void actionPerformed(ActionEvent e) {
             target.getImage().apply(new Flip(true));
             target.repaint();
@@ -76,9 +76,35 @@ public class TransformationActions {
         HorizontalFlipAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
             super(name, icon, desc, mnemonic);
         }
-    
+
         public void actionPerformed(ActionEvent e) {
             target.getImage().apply(new Flip(false));
+            target.repaint();
+            target.getParent().revalidate();
+        }
+    }
+
+    public class ResizeAction extends ImageAction {
+
+        ResizeAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+            super(name, icon, desc, mnemonic);
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            int ScaleFactor = 1;
+
+            SpinnerNumberModel sharpNumModel = new SpinnerNumberModel(ScaleFactor, 1, 5, 1);
+            JSpinner sharpnessSpinner = new JSpinner(sharpNumModel);
+            float option = JOptionPane.showOptionDialog(null, sharpnessSpinner, "Enter Image Scale factor",
+                    JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+
+            if (option == JOptionPane.CANCEL_OPTION) {
+                return;
+            } else if (option == JOptionPane.OK_OPTION) {
+                ScaleFactor = sharpNumModel.getNumber().intValue();
+            }
+
+            target.getImage().apply(new ResizeImage(ScaleFactor));
             target.repaint();
             target.getParent().revalidate();
         }
